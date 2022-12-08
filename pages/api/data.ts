6eb49737ea5c14c072010d146e4d6dc670259d67
newswim <ofenixculpa@gmail.com>
2022-12-07 20:49:49 -0600

    (feat) parse combinedCases json

--- pages/api/data.ts
+++ pages/api/data.ts
@@ -1,60 +1,31 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-import ChargeEvent from '../../models/ChargeEvent'
-import Charge, { RawCharge } from '../../models/Charge'
-
-const COMBINED_CHARGES_URL = 'https://github.com/open-austin/indigent-stats-frontend/raw/main/data/combined.json'
+import { combinedDataSchema } from '../../models/schemas'
+import { groupBy } from '../../lib/array'
 
 export const config = {
-  api: {
-    responseLimit: '20mb',
-    bodyParser: {
-      sizeLimit: '20mb',
+    api: {
+        responseLimit: '20mb',
     },
-  },
 }
 
-const combinedItems = (arr = []) => {
-    const res = arr.reduce(
-        (
-            acc: Array<RawCharge & { count: number, events: Array<ChargeEvent> }>,
-            obj: RawCharge & { count: number, events: Array<ChargeEvent> }
-        ) => {
-            let found = false
-            for (let i = 0; i < acc.length; i++) {
-                if (acc[i].case_number === obj.case_number) {
-                    found = true
-                    acc[i].count++
-                    const e = new ChargeEvent(obj)
-                    acc[i].events.push(e)
-                }
-            }
-            if (!found) {
-                obj.count = 1
-                const event = new ChargeEvent(obj)
-                obj.events = [event]
-                acc.push(obj)
-            }
-            return acc
-        },
-        []
-    )
-    return res
-}
-
-// TODO: Host JSON externally
 export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse
 ) {
     const file = await fetch(COMBINED_CHARGES_URL)
     const json = await file.json()
-    const results = combinedItems(json?.results)
 
-    const charges: Array<Charge> = []
-    results.forEach((result: RawCharge) => {
-        const charge = new Charge(result)
-        charges.push(charge)
-    })
+    const charges = combinedDataSchema.safeParse(json.results)
 
-    res.status(200).json(JSON.stringify(charges))
+    if (charges.success) {
+        const payload = groupBy(charges.data)((a) => a.case_number)
+
+        res.status(200).json(payload)
+    } else {
+        console.log('Parse Error:\n', charges.error.issues)
+        res.status(203).json(JSON.stringify(charges.error.issues))
+    }
 }
+
+const COMBINED_CHARGES_URL =
+    'https://github.com/open-austin/indigent-stats-frontend/raw/main/data/combined.json'
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,40 +1,47 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import { Suspense } from 'react'
 import useSWR from 'swr'
 import BarChart from '../components/BarChart'
+import { Loading } from '../components/Loading'
 import StackedBarChart from '../components/StackedBarChart'
+import { groupedChargesSchema } from '../models/schemas'
 import styles from '../styles/Home.module.css'
 
 const fetcher = (url: string) => fetch(url).then((res) => res.json())
 
 export default function Home() {
-  const { data, error } = useSWR('/api/data', fetcher)
+    const { data, error } = useSWR('/api/data', fetcher)
 
-  if (error) {
-    return <div>{error}</div>
-  }
+    const parsed = groupedChargesSchema.safeParse(data)
+
+    if (!parsed.success) {
+        return (
+            <div>
+                <pre>{JSON.stringify(parsed.error.issues, null, 2)}</pre>
+            </div>
+        )
+    }
 
     return (
         <div className={styles.container}>
             <Head>
                 <title>Open Austin | Indigent Defense Stats</title>
-                <meta
-                    name="description"
-                    content="Indigent Defense Stats"
-                />
+                <meta name="description" content="Indigent Defense Stats" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
 
             <main className={styles.main}>
-              <div className={styles.charts}>
-                <BarChart data={data} />
-                <StackedBarChart data={data} />
-              </div>
+                {/* <Suspense fallback={<Loading />}> */}
+
+                <div className={styles.charts}>
+                    {/* <BarChart data={data} /> */}
+                    <StackedBarChart data={parsed.data} />
+                </div>
+                {/* </Suspense> */}
             </main>
 
-            <footer className={styles.footer}>
-                Open Austin 2022
-            </footer>
+            <footer className={styles.footer}>Open Austin 2022</footer>
         </div>
     )
 }
