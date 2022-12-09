b51df090415f3faa40d80afeb251137c58498bbc
newswim <ofenixculpa@gmail.com>
2022-12-09 16:20:16 -0600

    (feat) update for new case-subset endpoint

---
+++ models/CaseEvent.ts
@@ -0,0 +1,9 @@
+import { z } from 'zod'
+
+// Note: we'd like to call this just `Event`, but cannot because
+// that's a reserved word in JavaScript. C'est la vie.
+export type CaseEvent = z.TypeOf<typeof caseEventSchema>
+
+export const caseEventSchema = z.array(
+    z.object({ event_date: z.string(), event_name: z.string().optional() })
+)
