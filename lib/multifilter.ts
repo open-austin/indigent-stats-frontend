09747244d871122395b6dd558a2e9df19e395bf9
Nidhi Reddy <nreddy216@gmail.com>
2022-12-13 18:32:56 -0500

    Add filter logic

--- lib/multifilter.ts
+++ lib/multifilter.ts
@@ -1,19 +1,38 @@
 import { IFilters } from '../components/Filter'
 import { Case } from '../models/Case'
 
+const filterSingleProperty = (
+    data: Array<Case>,
+    filter: keyof IFilters,
+    filters: IFilters
+) => {
+    if (filters[filter] === 'All') {
+      return data
+    }
+
+    return data.filter(
+        (d) => d.filters && d.filters[filter]?.includes(filters[filter])
+    )
+}
+
 // Filters data with multiple parameters
 const multifilter = (data: Array<Case>, filters: IFilters) => {
-    return filters.motions !== 'All'
-        ? data.filter((d) => d.filters?.motions?.includes(filters.motions))
-              .length
-        : data.reduce(
-              (a, b) =>
-                  a +
-                  (b.filters?.motions?.length && b.filters?.motions?.length >= 1
-                      ? 1
-                      : 0),
-              0
-          )
+    let filteredData =
+        filters.motions !== 'All'
+            ? data.filter((d) => d.filters?.motions?.includes(filters.motions))
+            : data.filter((d) => !!d.filters?.motions?.length)
+
+    for (const filter in filters) {
+        if (filter !== 'motions') {
+            filteredData = filterSingleProperty(
+                filteredData,
+                filter as keyof IFilters,
+                filters
+            )
+        }
+    }
+
+    return filteredData.length
 }
 
 export default multifilter
