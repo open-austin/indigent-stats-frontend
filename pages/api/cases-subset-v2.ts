6e10852a2b75c7119e8abcc890c5a2ca96aaf2de
Nidhi Reddy <nreddy216@gmail.com>
2022-12-21 10:47:50 -0600

    Chnage back to using sample

--- pages/api/cases-subset-v2.ts
+++ pages/api/cases-subset-v2.ts
@@ -20,6 +20,7 @@ export default async function handler(
     res: NextApiResponse
 ) {
     const jsonDirectory = path.join(process.cwd(), 'data');
-    const fileContents = await fs.readFile(jsonDirectory + '/nested_cases.json', 'utf8');
+    // TODO: How to improve filtering performance for larger sample size?
+    const fileContents = await fs.readFile(jsonDirectory + '/nested_cases_sample.json', 'utf8');
     res.status(200).json(fileContents);
 }
