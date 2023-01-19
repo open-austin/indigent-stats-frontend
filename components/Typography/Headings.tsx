d07f6ef8c6e34ddb607abf87bb7c54b3409173ab
Nidhi Reddy <nreddy216@gmail.com>
2023-01-19 12:48:54 -0600

    Add media query

--- components/Typography/Headings.tsx
+++ components/Typography/Headings.tsx
@@ -1,8 +1,19 @@
 import styled from 'styled-components'
+import { bp } from '../../lib/breakpoints'
 import { colors } from '../../lib/colors'
 
+export const H1 = styled.h1`
+    font-size: 2rem;
+    line-height: 1.5;
+    color: ${colors.openAustinOrange};
+
+    @media ${bp.lg} {
+        font-size: 3rem;
+    }
+`
+
 export const H2 = styled.h2`
-  font-size: 2.2rem;
-  line-height: 1.25;
-  color: ${colors.openAustinOrange};
+    font-size: 2.2rem;
+    line-height: 1.25;
+    color: ${colors.openAustinOrange};
 `
