846397b7a0ffcbc08acf6c0f7d3fbbc5d69f9010
Nidhi Reddy <nreddy216@gmail.com>
2023-01-24 00:21:14 -0600

    Update years chart, fix minor css, rotate animated bg

---
+++ components/FullWidthContainer.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import styled from 'styled-components'
+import { Container } from './Container'
+
+interface Props {
+    bgColor?: string
+    transparent?: boolean
+    children: React.ReactNode
+}
+
+const Wrapper = styled.section<Props>`
+    padding: 2rem;
+    margin: -2rem -4rem 0 -4rem;
+    width: calc(100% + 8rem);
+    background-color: ${(props) =>
+        props.bgColor && props.transparent
+            ? `${props.bgColor}AA`
+            : props.bgColor};
+`
+
+export const FullWidthContainer = ({ bgColor, children, transparent }: Props) => {
+    return (
+        <Wrapper bgColor={bgColor} transparent={transparent}>
+            <Container>
+                {children}
+            </Container>
+        </Wrapper>
+    )
+}
