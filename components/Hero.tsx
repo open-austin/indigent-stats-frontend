3726fc4c1011dcaf3390d3a435239fc8fbdfbf88
Nidhi Reddy <nreddy216@gmail.com>
2023-01-23 23:26:25 -0600

    Fix styled components ssr issue, other minor style fixes

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -6,9 +6,10 @@ import { Banner } from './Banner'
 import { Container, TextContainer } from './Container'
 import { Section } from './Section'
 import { Highlight } from './Typography/Highlight'
-import { H1 } from './Typography/Headings'
+import { H1, H3 } from './Typography/Headings'
 import { bp } from '../lib/breakpoints'
-import useMediaQuery from '../lib/hooks/useMediaQuery'
+import openAustinSvg from '../public/open-austin-logo.svg'
+import { Paragraph } from './Typography/Body'
 
 const Wrapper = styled.div`
     position: relative;
@@ -17,12 +18,16 @@ const Wrapper = styled.div`
     right: -2rem;
     top: -2rem;
     width: calc(100% + 4rem);
-    padding: 10rem 4rem 0 4rem;
+    padding: 6rem 2rem 0 2rem;
     display: flex;
     align-items: center;
     flex-direction: column;
     color: ${colors.text};
     font-size: 1.25rem;
+
+    @media ${bp.lg} {
+        padding: 10rem 4rem 0 4rem;
+    }
 `
 const Background = styled.div`
     width: 100%;
@@ -36,24 +41,52 @@ const Background = styled.div`
 `
 
 const HeaderWrapper = styled.div`
-    margin: 0 auto 6rem;
+    margin: 0 auto;
     line-height: 2;
     display: flex;
-    gap: 3rem;
+    flex-direction: column-reverse;
+    gap: 1rem;
+    align-items: flex-start;
+    text-align: center;
 
     @media ${bp.lg} {
-        grid-template-columns: 2fr 1fr;
+        margin-bottom: 6rem;
+        flex-direction: row;
         gap: 6rem;
+        text-align: left;
     }
 `
 
 const Heading = styled(H1)`
     padding: 1rem 0;
-    margin: 0 auto 0 2rem;
     color: ${colors.openAustinOrange};
+    text-align: center;
+    flex-basis: 60%;
+
+    @media ${bp.lg} {
+        margin: 0 auto 0 2rem;
+        text-align: left;
+    }
 `
 
-const Subheading = styled.h3``
+const ImageWrapper = styled.div`
+    flex-grow: 0;
+    flex-basis: 40%;
+    display: flex;
+    justify-content: center;
+
+    img {
+        width: 100%;
+        max-width: 25%;
+        height: auto;
+
+        @media ${bp.lg} {
+            max-width: none;
+            max-height: 100%;
+            width: auto;
+        }
+    }
+`
 
 const Description = styled.div`
     max-width: 80%;
@@ -61,12 +94,15 @@ const Description = styled.div`
     margin-left: 2rem;
 
     p {
-        font-size: 1.5rem;
+        text-align: center;
+
+        @media ${bp.lg} {
+            text-align: left;
+        }
     }
 `
 
 export const Hero = () => {
-    const isLg = useMediaQuery('lg')
     const headingText =
         'Visualizing the impact of access to appointed counsel in Texas criminal cases'
 
@@ -78,21 +114,14 @@ export const Hero = () => {
                 </Background>
                 <Container>
                     <HeaderWrapper>
-                        <div>
-                            <Heading>{headingText}</Heading>
-                        </div>
-                        <div>
-                            <Image
-                                src="/open-austin-logo.svg"
-                                alt="Open Austin logo"
-                                width={isLg ? 200 : 100}
-                                height={isLg ? 200 : 100}
-                            />
-                        </div>
+                        <Heading>{headingText}</Heading>
+                        <ImageWrapper>
+                            <Image src={openAustinSvg} alt="Open Austin logo" />
+                        </ImageWrapper>
                     </HeaderWrapper>
                     <Section>
                         <Description>
-                            <p>
+                            <Paragraph>
                                 Our organization,{' '}
                                 <a
                                     href="https://www.open-austin.org"
@@ -108,7 +137,7 @@ export const Hero = () => {
                                 physical court records, which made it difficult
                                 for policy-makers and journalists to get a big
                                 picture view of statewide criminal cases.
-                            </p>
+                            </Paragraph>
                             <br></br>
                             <br></br>
                             <br></br>
@@ -119,16 +148,15 @@ export const Hero = () => {
                     bgColor={colors.blueNavyTransparent}
                     color={colors.white}
                 >
-                    <Subheading>
+                    <H3>
                         <TextContainer>
                             Through this research, we learned that folks who
                             can&apos;t afford appointed counsel are{' '}
-                            <u>less likely</u>{' '}
-                            to have adequate{' '}
+                            <u>less likely</u> to have adequate{' '}
                             <Highlight>evidence of representation</Highlight> in
                             their trials.
                         </TextContainer>
-                    </Subheading>
+                    </H3>
                 </Banner>
             </Wrapper>
         </Section>
--- components/Typography/Headings.tsx
+++ components/Typography/Headings.tsx
@@ -13,7 +13,25 @@ export const H1 = styled.h1`
 `
 
 export const H2 = styled.h2`
-    font-size: 2.2rem;
+    font-size: 1.6rem;
     line-height: 1.25;
     color: ${colors.openAustinOrange};
+    max-width: 25rem;
+
+    @media ${bp.md} {
+        max-width: 80%;
+    }
+
+    @media ${bp.lg} {
+        font-size: 2.2rem;
+    }
+`
+
+export const H3 = styled.h3`
+    font-size: 1.5rem;
+    line-height: 1.75;
+
+    @media ${bp.lg} {
+        font-size: 2rem;
+    }
 `
