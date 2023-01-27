d18506420acd34f514cedcd5f726e6004bc56382
Nidhi Reddy <nreddy216@gmail.com>
2023-01-27 12:43:36 -0600

    Fade in sections, small css updates

---
+++ lib/hooks/usePrefersReducedMotion.ts
@@ -0,0 +1,22 @@
+import { useState, useEffect } from 'react'
+
+// if user does not want animations
+function usePrefersReducedMotion() {
+    const [matches, setMatches] = useState(false)
+
+    useEffect(() => {
+        const media = window.matchMedia('(prefers-reduced-motion: reduce)')
+        if (media.matches !== matches) {
+            setMatches(media.matches)
+        }
+        const listener = () => {
+            setMatches(media.matches)
+        }
+        media.addEventListener("change", listener)
+        return () => media.removeEventListener("change", listener)
+    }, [matches])
+
+    return matches
+}
+
+export default usePrefersReducedMotion
