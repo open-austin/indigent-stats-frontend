f757467270d34cddc8698de958da2651a7992fe2
newswim <ofenixculpa@gmail.com>
2023-06-26 00:57:57 -0500

    fix bad import

--- pages/api/kysely-test.ts
+++ pages/api/kysely-test.ts
@@ -1,24 +1,28 @@
-import { CosmosClient } from "@azure/cosmos";
-import { Kysely, sql } from "kysely";
-import { NextApiRequest, NextApiResponse } from "next";
-import { CosmosDialect } from "../../lib/adapter/CosmosDialect";
+import { CosmosClient } from '@azure/cosmos'
+import { Kysely, sql } from 'kysely'
+import { NextApiRequest, NextApiResponse } from 'next'
+import { CosmosDialect } from '../../lib/adapter/CosmosDialect'
+import { Database } from '../../lib/schema'
 
 const db = new Kysely<Database>({
     // PostgresDialect requires the Cursor dependency
     dialect: new CosmosDialect({
-      client: new CosmosClient({endpoint: process.env.COSMOSDB_ENDPOINT!, key: process.env.COSMOSDB_KEY!}),
-      database: 'cases-json-db',
-      container: 'clean-cases',
+        client: new CosmosClient({
+            endpoint: process.env.COSMOSDB_ENDPOINT!,
+            key: process.env.COSMOSDB_KEY!,
+        }),
+        database: 'cases-json-db',
+        container: 'clean-cases',
     }),
     // MysqlDialect doesn't require any special configuration
-  })
-
+})
 
 export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse
 ) {
-    const result = await db.selectFrom('c')
+    const result = await db
+        .selectFrom('case')
         .select(sql`count(1)`.as('count'))
         .execute()
     res.status(200).json({ result })
